8663f88fc5c8f339a429e67530b15f5287296b45
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-04-24 17:53:31 +0700

    update check internet

--- abc_learning_app/lib/main.dart
+++ abc_learning_app/lib/main.dart
@@ -17,6 +17,7 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+  DependencyInjection.init();
 }
 
 class MyApp extends StatefulWidget {
